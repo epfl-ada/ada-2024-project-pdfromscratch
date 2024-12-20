32f7c6ae6c8b1b3d0e2053fa6f6c60691ceee3e9
jnsffrt <jean.siffert@epfl.ch>
2024-12-20 20:31:25 +0100

    NLP interpretations

--- src/utils/nlp.py
+++ src/utils/nlp.py
@@ -1,7 +1,55 @@
 import pandas as pd
 import numpy as np
+import torch
+from tqdm import tqdm
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from torch.nn.functional import softmax
 import networkx as nx
 from statsmodels.stats.multicomp import pairwise_tukeyhsd
+
+
+def compute_weighted_rating(df_predictions: pd.DataFrame) -> pd.DataFrame:
+    def _compute_weighted_rating(rating_weights: pd.Series):
+        prediction = int(rating_weights.idxmax())
+
+        values = [prediction]
+        weights = [rating_weights.max()]
+
+        if prediction > 1:
+            values.append(prediction - 1)
+            weights.append(rating_weights.loc[str(prediction - 1)])
+
+        if prediction < 5:
+            values.append(prediction + 1)
+            weights.append(rating_weights.loc[str(prediction + 1)])
+
+        return np.average(values, weights=weights), np.sum(weights)
+    return df_predictions.apply(_compute_weighted_rating, axis=1)
+
+
+def predict_rating(reviews: pd.Series) -> pd.DataFrame:
+    HUGGING_FACE_MODEL = "nlptown/bert-base-multilingual-uncased-sentiment"
+
+    tokenizer = AutoTokenizer.from_pretrained(HUGGING_FACE_MODEL)
+    model = AutoModelForSequenceClassification.from_pretrained(HUGGING_FACE_MODEL)
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+
+    encoded_input = tokenizer(reviews.tolist(), padding=True, truncation=True, max_length=512, return_tensors='pt')
+    encoded_input = {key: tensor.to(device) for key, tensor in encoded_input.items()}
+    with torch.no_grad():
+        output = model(**encoded_input)
+    scores = softmax(output.logits, dim=1)
+
+    df_predictions = pd.DataFrame([
+        *scores.cpu().numpy()
+    ], columns=['1', '2', '3', '4', '5'], index=reviews.index)
+
+    df_predictions = pd.merge(df_predictions, compute_weighted_rating(df_predictions), left_index=True, right_index=True)
+    df_predictions.columns = ['1', '2', '3', '4', '5', 'nlp_weighted_rating', 'nlp_weighted_confidence']
+    return df_predictions
+
 
 def tukey_test(dataframe: pd.DataFrame, group_column: str, value_column: str, alpha: float = 0.05) -> pd.DataFrame:
     tukey = pairwise_tukeyhsd(endog=dataframe[value_column], groups=dataframe[group_column], alpha=alpha)
